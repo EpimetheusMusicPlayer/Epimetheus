b5bd609bb8b9a8fc67bec7625c3b2310c714f6bc
Josh Leivenzon <joshleivenzon@outlook.com>
2019-04-27 22:18:26 +1000

    Fix another stopping bug

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -22,8 +22,7 @@ class EpimetheusModel extends Model {
   MusicProvider _currentMusicProvider;
   MusicProvider get currentMusicProvider => _currentMusicProvider;
   set currentMusicProvider(musicProvider) {
-    if (musicProvider == null) return;
-    launchMusicProvider(user, musicProvider);
+    if (musicProvider != null) launchMusicProvider(user, musicProvider);
     _currentMusicProvider = musicProvider;
     notifyListeners();
   }
