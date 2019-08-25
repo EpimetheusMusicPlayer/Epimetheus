cc7fd74909c34ac18cb90a67e18a28df090b9413
hacker1024 <joshleivenzon@outlook.com>
2019-08-25 13:02:29 +1000

    Add a navigation drawer

--- lib/pages/collection/collection_page.dart
+++ lib/pages/collection/collection_page.dart
@@ -1,10 +1,14 @@
 import 'package:epimetheus/models/user.dart';
+import 'package:epimetheus/pages/navigation_drawer.dart';
 import 'package:flutter/material.dart';
 
 class CollectionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const NavigationDrawer(
+        currentRouteName: '/collection',
+      ),
       appBar: AppBar(
         title: const Text('My Collection'),
       ),
---
+++ lib/pages/navigation_drawer.dart
@@ -0,0 +1,76 @@
+import 'package:epimetheus/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class NavigationDrawerItem {
+  final String name;
+  final String routeName;
+  final IconData iconData;
+
+  const NavigationDrawerItem({
+    @required this.name,
+    @required this.routeName,
+    @required this.iconData,
+  });
+}
+
+class NavigationDrawer extends StatelessWidget {
+  final String currentRouteName;
+
+  const NavigationDrawer({this.currentRouteName});
+
+  static const _navigationDrawItems = const <NavigationDrawerItem>[
+    const NavigationDrawerItem(name: 'My Collection', routeName: '/collection', iconData: Icons.library_music),
+    const NavigationDrawerItem(name: 'Now Playing', routeName: '/now-playing', iconData: Icons.queue_music),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          ScopedModelDescendant<UserModel>(
+            builder: (context, child, model) {
+              return UserAccountsDrawerHeader(
+                accountName: Text(model.user.username),
+                accountEmail: Text(model.user.email),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(model.user.profileImageUrl),
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center,
+                    colorFilter: ColorFilter.mode(Colors.black54, BlendMode.multiply),
+                  ),
+                ),
+              );
+            },
+          ),
+          Expanded(
+            child: ListTileTheme.merge(
+              selectedColor: const Color(0xFF820096), // Dark accent color
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: _navigationDrawItems.map<ListTile>(
+                  (item) {
+                    return ListTile(
+                      leading: Icon(item.iconData),
+                      title: Text(item.name),
+                      selected: item.routeName == currentRouteName,
+                      onTap: () {
+                        if (item.routeName != currentRouteName) {
+                          Navigator.of(context)
+                            ..pop()
+                            ..pushReplacementNamed(item.routeName);
+                        }
+                      },
+                    );
+                  },
+                ).toList(growable: false),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
