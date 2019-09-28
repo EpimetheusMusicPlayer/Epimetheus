cc922e4ebe6a6cf101b0a94863f900125f014548
Joshua Leivenzon <joshleivenzon@outlook.com>
2019-09-28 22:15:40 +1000

    Make the all tab reactive

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:epimetheus/libepimetheus/authentication.dart';
+import 'package:epimetheus/libepimetheus/exceptions.dart';
 import 'package:epimetheus/libepimetheus/stations.dart' as api;
 import 'package:flutter/widgets.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
@@ -6,15 +9,39 @@ import 'package:scoped_model/scoped_model.dart';
 
 class CollectionModel extends Model {
   List<api.Station> _stations;
+  bool _hasError = false;
 
   void clear() {
     _stations = null;
     notifyListeners();
   }
 
+  bool get hasError => _hasError;
+
   Future<void> refreshStations(User user) async {
+    if (_stations != null || _hasError) {
+      if (_stations != null) {
+        _stations = null;
+      }
+      if (_hasError) {
+        _hasError = false;
+      }
+
+      notifyListeners();
+    }
+
     // Download the stations list
-    _stations = await api.getStations(user, true);
+    try {
+      _stations = await api.getStations(user, true);
+    } on SocketException {
+      _hasError = true;
+      notifyListeners();
+      return;
+    } on PandoraException {
+      _hasError = true;
+      notifyListeners();
+      return;
+    }
 
     // Cache the station art
     final cacheManager = DefaultCacheManager();
@@ -29,6 +56,11 @@ class CollectionModel extends Model {
     if (_stations == null) {
       await refreshStations(user);
     }
+    return _stations;
+  }
+
+  List<api.Station> asyncStations(User user) {
+    if (_stations == null) refreshStations(user);
     return _stations;
   }
 
--- lib/pages/collection/all_tab.dart
+++ lib/pages/collection/all_tab.dart
@@ -1,86 +1,70 @@
-import 'dart:io';
-
-import 'package:epimetheus/libepimetheus/exceptions.dart';
 import 'package:epimetheus/libepimetheus/stations.dart';
 import 'package:epimetheus/models/collection.dart';
 import 'package:epimetheus/models/user.dart';
 import 'package:epimetheus/widgets/art_displays.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class AllTab extends StatefulWidget {
-  @override
-  _AllTabState createState() => _AllTabState();
-}
-
-class _AllTabState extends State<AllTab> with AutomaticKeepAliveClientMixin<AllTab> {
-  @override
-  bool get wantKeepAlive => true;
-
-  List<Station> _stations;
-
-  Future<bool> _loadData() async {
-    try {
-      _stations = await CollectionModel.of(context).getStations(UserModel.of(context).user);
-      return true;
-    } on SocketException {
-      return false;
-    } on PandoraException {
-      return false;
-    }
-  }
-
+class AllTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    return FutureBuilder<bool>(
-      future: _loadData(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
+    return ScopedModelDescendant<CollectionModel>(
+      builder: (context, child, model) {
+        // Gets the current downloaded station list, and if it doesn't exist, starts the download.
+        final List<Station> stations = model.asyncStations(UserModel.of(context).user);
+
+        if (model.hasError) {
+          return Center(
+            child: const Text(
+              'There was an error fetching your stuff. Please try again.',
+            ),
+          );
+        }
+
+        if (stations == null) {
           return Center(
             child: CircularProgressIndicator(),
           );
-        } else {
-          if (snapshot.data) {
-            final stationArtUrls = List<String>(_stations.length);
-            final stationLabels = List<String>(_stations.length);
-
-            for (int i = 0; i < _stations.length; i++) {
-              final station = _stations[i];
-              stationArtUrls[i] = station.getArtUrl(500);
-              stationLabels[i] = station.title;
-            }
-
-            return ListView(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: const Text(
-                    'My Stations',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                    ),
-                  ),
-                ),
-                ArtTileCarousel(
-                  artUrls: stationArtUrls,
-                  labels: stationLabels,
-                  onTap: (index) {
-                    print('Station tapped on $index');
-                  },
-                ),
-              ],
-            );
-          } else {
-            return Center(
-              child: const Text(
-                'There was an error fetching your stuff. Please try again.',
-              ),
-            );
-          }
         }
+
+        return buildMainContent(stations);
       },
+    );
+  }
+
+  Widget buildMainContent(List<Station> stations) {
+    // Split the station list into two lists of art urls and labels.
+    final stationArtUrls = List<String>(stations.length);
+    final stationLabels = List<String>(stations.length);
+
+    for (int i = 0; i < stations.length; i++) {
+      final station = stations[i];
+      stationArtUrls[i] = station.getArtUrl(500);
+      stationLabels[i] = station.title;
+    }
+
+    // Main content.
+    return ListView(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: const Text(
+            'My Stations',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+            ),
+          ),
+        ),
+        ArtTileCarousel(
+          artUrls: stationArtUrls,
+          labels: stationLabels,
+          onTap: (index) {
+            print('Station tapped on $index');
+          },
+        ),
+      ],
     );
   }
 }
