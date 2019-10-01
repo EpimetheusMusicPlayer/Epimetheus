dfd6f618ee090c8e2de07ae302ecd0f3b83ff039
Joshua Leivenzon <joshleivenzon@outlook.com>
2019-10-01 21:15:43 +1000

    Finish the track list tile widget

---
+++ lib/audio/launch_helpers.dart
@@ -0,0 +1,10 @@
+import 'package:epimetheus/libepimetheus/songs.dart';
+import 'package:epimetheus/libepimetheus/stations.dart';
+
+void launchStation(Station station) {
+  print('Launching station: ${station.title}');
+}
+
+void launchTrack(Track track) {
+  print('Launching track: ${track.title}');
+}
--- lib/widgets/playable/track.dart
+++ lib/widgets/playable/track.dart
@@ -1,6 +1,56 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:epimetheus/audio/launch_helpers.dart';
 import 'package:epimetheus/libepimetheus/songs.dart';
+import 'package:epimetheus/widgets/flags/explicit.dart';
 import 'package:flutter/material.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
+
+class TrackTextFormatting {
+  static String formatMinutes(int minutes) => '${(minutes / 60).floor()}:${minutes.remainder(60).toString().padLeft(2, '0')}';
+}
+
+class TrackInfoText extends StatelessWidget {
+  final String title;
+  final String album;
+  final String artist;
+
+  const TrackInfoText({
+    @required this.title,
+    @required this.album,
+    @required this.artist,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          softWrap: false,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Text(
+          artist,
+          softWrap: false,
+          overflow: TextOverflow.ellipsis,
+        ),
+        Text(
+          album,
+          softWrap: false,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ],
+    );
+  }
+}
 
 class TrackListTile extends StatelessWidget {
   final Track track;
@@ -9,32 +59,64 @@ class TrackListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        children: <Widget>[
-          CachedNetworkImage(
-            height: 56,
-            imageUrl: track.getArtUrl(500),
-            placeholder: (context, imageUrl) => Image.asset(
-              'assets/music_note.png',
+    return SizedBox(
+      height: 88,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          top: 16,
+          bottom: 16,
+        ),
+        child: Row(
+          children: <Widget>[
+            CachedNetworkImage(
               height: 56,
+              imageUrl: track.getArtUrl(500),
+              placeholder: (context, imageUrl) => Image.asset(
+                'assets/music_note.png',
+                height: 56,
+              ),
+              placeholderFadeInDuration: const Duration(milliseconds: 500),
             ),
-            placeholderFadeInDuration: const Duration(milliseconds: 500),
-          ),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            const SizedBox(width: 16),
+            Expanded(
+              child: TrackInfoText(
+                title: track.title,
+                album: track.albumTitle,
+                artist: track.artistTitle,
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
-                Text(track.title),
-                Text(track.artistTitle),
-                Text(track.albumTitle),
+                if (track.explicitness == TrackExplicitness.explicit)
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: const Explicit(),
+                    ),
+                  ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        TrackTextFormatting.formatMinutes(track.duration),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
-          ),
-          Text('${(track.duration / 60).floor()}:${track.duration.remainder(60).toString().padLeft(2, '0')}'),
-        ],
+            IconButton(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              icon: const Icon(OMIcons.playArrow),
+              tooltip: 'Play',
+              onPressed: () => launchTrack(track),
+            ),
+          ],
+        ),
       ),
     );
   }
