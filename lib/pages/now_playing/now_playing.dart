423edf9326b23b710a5cfd0833a095c83d91a4c0
Joshua Leivenzon <joshleivenzon@outlook.com>
2019-10-05 22:21:53 +1000

    Move the media control bar into its own file, and check that it's a descendant of AudioServiceDisplay on build.

--- lib/pages/now_playing/now_playing.dart
+++ lib/pages/now_playing/now_playing.dart
@@ -1,6 +1,6 @@
-import 'package:audio_service/audio_service.dart';
 import 'package:epimetheus/pages/navigation_drawer.dart';
-import 'package:epimetheus/widgets/misc/audio_service_display.dart';
+import 'package:epimetheus/widgets/audio/audio_service_display.dart';
+import 'package:epimetheus/widgets/audio/media_control_bar.dart';
 import 'package:flutter/material.dart';
 
 class NowPlayingPage extends StatelessWidget {
@@ -15,47 +15,6 @@ class NowPlayingPage extends StatelessWidget {
       ),
       body: AudioServiceDisplay(
         child: MediaControlBar(),
-      ),
-    );
-  }
-}
-
-class MediaControlBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          const IconButton(
-            icon: const Icon(Icons.stop),
-            onPressed: AudioService.stop,
-          ),
-          const IconButton(
-            icon: const Icon(Icons.fast_rewind),
-            onPressed: AudioService.rewind,
-          ),
-          StreamBuilder<PlaybackState>(
-            initialData: AudioService.playbackState,
-            stream: AudioService.playbackStateStream,
-            builder: (context, snapshot) {
-              print('Buildin\'');
-              final paused = snapshot.data.basicState == BasicPlaybackState.paused;
-              return IconButton(
-                icon: Icon(paused ? Icons.play_arrow : Icons.pause),
-                onPressed: paused ? AudioService.play : AudioService.pause,
-              );
-            },
-          ),
-          const IconButton(
-            icon: const Icon(Icons.fast_forward),
-            onPressed: AudioService.fastForward,
-          ),
-          const IconButton(
-            icon: const Icon(Icons.skip_next),
-            onPressed: AudioService.skipToNext,
-          ),
-        ],
       ),
     );
   }
